5b29afcb88cdae6cf72800527789072158c0c8af
sakinaghayeva <sekinea006@gmail.com>
2024-10-25 12:52:27 +0400

    salam

---
+++ src/main/kotlin/org/example/enyeni/EnyeniApplication.kt
@@ -0,0 +1,11 @@
+package org.example.enyeni
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EnyeniApplication
+
+fun main(args: Array<String>) {
+    runApplication<EnyeniApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/enyeni/EnyeniApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.enyeni
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EnyeniApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
